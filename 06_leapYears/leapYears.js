21251c7f47e8d9ea055f1dcc38919fc028f115e4
chalex <177287665+chalexmarmol@users.noreply.github.com>
2024-12-09 18:37:27 +0800

    done js-6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = (num) => {
+	if (num % 400 === 0) return true;
+	if (num % 100 !== 0 && num % 4 === 0) return true;
+	return false;
 };
 
 // Do not edit below this line
