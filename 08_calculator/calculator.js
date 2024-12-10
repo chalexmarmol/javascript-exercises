0b481632c30263616f9c57b75c09a68baf4dbc5d
Chalex <177287665+chalexmarmol@users.noreply.github.com>
2024-12-10 11:00:45 +0800

    done js-8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,39 @@
-const add = function() {
-	
+const add = function (n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function (n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+	return arr.reduce((current, value) => current + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+	return arr.reduce((product, value) => product * value);
 };
 
-const power = function() {
-	
+const power = function (num, pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+	let product = 1;
+
+	for (let i = num; i > 0; i--) {
+		product *= i;
+	}
+
+	return product;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
