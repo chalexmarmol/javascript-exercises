2964e004376843c157b33d1f3884e3de7d67b723
Chalex <177287665+chalexmarmol@users.noreply.github.com>
2024-12-10 11:55:54 +0800

    done js-9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = (str) => {
+	let alphanumerical = 'abcdefghijklmnopqrstuvwxyz1234567890';
 
+	const cleanedString = str
+		.toLowerCase()
+		.split('')
+		.filter((character) => alphanumerical.includes(character))
+		.join('');
+
+	const reverseString = cleanedString.split('').reverse().join('');
+	return cleanedString === reverseString;
 };
+console.log(palindromes('ZZZZ car, a man, a maracaz.'));
 
 // Do not edit below this line
 module.exports = palindromes;
